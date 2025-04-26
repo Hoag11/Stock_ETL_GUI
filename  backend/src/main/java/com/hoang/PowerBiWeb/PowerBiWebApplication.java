06618b9afbd8b3958b1e93ca47fc4b658b752c27
Hoag11 <nghuyhoang1101@gmail.com>
2025-04-26 18:28:27 +0700

    update backend

---
+++  backend/src/main/java/com/hoang/PowerBiWeb/PowerBiWebApplication.java
@@ -0,0 +1,13 @@
+package com.hoang.PowerBiWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerBiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerBiWebApplication.class, args);
+	}
+
+}
---
+++  backend/src/test/java/com/hoang/PowerBiWeb/PowerBiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoang.PowerBiWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerBiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
