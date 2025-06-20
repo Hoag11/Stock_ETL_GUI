f752014396c82205503881e22f798374ae45d1cd
Liber72 <datht.b22tc027@stu.ptit.edu.vn>
2025-06-20 22:35:09 +0700

    update profile

--- front-end/src/pages/profile/Profile.tsx
+++ front-end/src/pages/profile/Profile.tsx
@@ -65,16 +65,8 @@ const Profile: React.FC = () => {
           
           <form onSubmit={handleSubmit} className="space-y-4">
             <div>
-              <label htmlFor="username" className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-                Username
-              </label>
-              <input
-                id="username"
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:text-white"
-              />
+              
+              
             </div>
             
             <div>
